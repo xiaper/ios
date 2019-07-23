3cce84a6077523f37cb9e952333e39ccb95f0afc
jackning <270580156@qq.com>
2019-07-23 17:00:37 +0800

    update

--- xiaper-demo/Pods/Target Support Files/Pods-bytedesk-ui-bduiTests/Pods-bytedesk-ui-bduiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bytedesk_ui_bduiTests : NSObject
-@end
-@implementation PodsDummy_Pods_bytedesk_ui_bduiTests
-@end
--- xiaper-demo/Pods/Target Support Files/Pods-bytedesk-ui/Pods-bytedesk-ui-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bytedesk_ui : NSObject
-@end
-@implementation PodsDummy_Pods_bytedesk_ui
-@end
